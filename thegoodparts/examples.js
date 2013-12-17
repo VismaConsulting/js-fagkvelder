ad8ad5afecc2e9c9ecf352ea01be878945a95910
andersem <andersems@gmail.com>
2013-12-17 22:49:13 +0100

    Legger inn eksempel på at + som både konkatenerer og adderer er dumt

---
+++ thegoodparts/examples.js
@@ -0,0 +1,11 @@
+//Bad parts
+
+// + both adds and concatenates
+
+// Expects 12hei, but gets 3hei
+// http://programmers.stackexchange.com/questions/90203/why-is-so-bad-for-concatenation
+var nonWantedBehavior = 1 + 2 + "hei";
+
+
+
+
